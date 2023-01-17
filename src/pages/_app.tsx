982261b6a38f4dfe8f2abe760c708c0d224311ca
Luis Fernando <luisfernandinho094@gmail.com>
2023-01-17 15:17:18 -0300

    feat: setup tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Poppins } from "@next/font/google";
+
+const poppins = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>
+        {`
+          :root {
+            --poppins-font: ${poppins.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+  );
 }
